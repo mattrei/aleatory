4b306d1798d75d49c816250eefceeb034157893e
Matthias Treitler <matthias.treitler@gmail.com>
2015-08-31 14:13:58 +0200

    Update data.rb

--- data.rb
+++ data.rb
@@ -23,24 +23,63 @@ define :get_executed do
   puts page.xpath('//tr').select{|x| x.children.to_a[17].text == 'Hispanic'}.count
 end
 
-get_executed
+#get_executed
 
-define :get_scheduled_executions do
-  agent = Mechanize.new
-  page = Nokogiri::HTML(open("#{URL}dr_scheduled_executions.html"))
 
-  page.xpath('//tr').to_a.drop(1).each do |tr|
-    a = tr.children.to_a
-    puts "#{a[7].text} #{a[9].text} Birth: #{a[11].text} Race: #{a[13].text} #{a[1].text}"
-    link =  "http://www.tdcj.state.tx.us/death_row/#{a[3].children[0]['href']}"
-    spage = Nokogiri::HTML(open(link))
-    img = spage.css('img.photo_border_black_right')
-    if !img.first.nil?
+
+arr = []
+all_scheduled = false
+in_thread(name: :scheduled_thread) do
+
+  
+    agent = Mechanize.new
+    page = Nokogiri::HTML(open("#{URL}dr_scheduled_executions.html"))
+    
+    page.xpath('//tr').to_a.drop(1).each do |tr|
+      a = tr.children.to_a
+      link =  "http://www.tdcj.state.tx.us/death_row/#{a[3].children[0]['href']}"
+      spage = Nokogiri::HTML(open(link))
+      img = spage.css('img.photo_border_black_right')
+      if !img.first.nil?
+        puts "#{a[7].text} #{a[9].text} Birth: #{a[11].text} Race: #{a[13].text} #{a[1].text}"
         encoded_image = Base64.encode64 agent.get("#{URL}/dr_info/#{img.first['src']}").body_io.string
         puts encoded_image
+        d = {}
+        d[:img] = encoded_image
+        d[:date] = a[1].text
+        arr.push(d)
+       puts "pushed"
+      end
+    end
+    all_scheduled = true
+ end
+
+
+
+  require 'osc-ruby'
+
+  def shader(endpoint, *args)
+    puts "sha"
+    @client ||= OSC::Client.new('localhost', 7400)
+    begin
+      args = args.map{|a| a.is_a?(Symbol) ? a.to_s : a}
+      @client.send(OSC::Message.new(endpoint, *args))
+    rescue Exception
+      puts "$!> Graphics not loaded"
     end
   end
-end
 
+  shader(:page, "/TheScheduled")
 
-get_scheduled_executions
+  live_loop :dance do
+    tick
+    
+    if all_scheduled
+    
+    print arr[look % arr.count]
+    
+      shader(:uniform, "img",  arr[look % arr.count][:img])
+      shader(:uniform, "date", arr[look % arr.count][:date])
+    end
+    sleep 2
+  end
