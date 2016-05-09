d762e3e59be7b67f4718895d857287c4c2749777
mattre <mat@quorum.at>
2016-05-09 23:38:46 +0200

    updated terrain

--- viz/src/Intro/terrain.js
+++ viz/src/Intro/terrain.js
@@ -2,6 +2,7 @@ const noise = new(require('noisejs').Noise)(Math.random())
 console.log(noise.perlin2(1, 2))
 
 const simplex = new(require('simplex-noise'))()
+const smoothstep = require('smoothstep')
 
 const random = require('random-float')
 const randomInt = require('random-int')
@@ -18,7 +19,8 @@ const VIS = 'terrain'
 const conf = {
   on: false,
   speed: 0.5,
-  mountainHeight: 0.5
+  mountainHeight: 0.5,
+  terrainHeight: 0.5
 }
 
 function terrain(scene, on = false) {
@@ -50,11 +52,14 @@ class Terrain {
     this.group = args.group
 
 
-
-    this.PLANE_WIDTH = 80
-    this.PLANE_DEPTH = 60
+    this.TERRAIN_HEIGHT = 2
+    this.MOUNTAIN_HEIGHT = 5
+    this.PLANE_WIDTH = 40
+    this.PLANE_DEPTH = 20
 
     this.planeMesh = null
+
+    this.mountains = []
 
     this.initPlane()
     this.initLights()
@@ -146,54 +151,6 @@ class Terrain {
       this.PLANE_WIDTH, this.PLANE_DEPTH)
     geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2))
 
-    const positions = geometry.attributes.position.array
-    const TERRAIN_HEIGHT = 1,
-      MOUNTAIN_HEIGHT = 10
-
-
-    const data = this.generateHeight(this.PLANE_WIDTH, this.PLANE_DEPTH)
-    for (var i = 0, j = 0; i < positions.length; i++, j += 3) {
-      positions[j + 1] += data[i] * TERRAIN_HEIGHT
-    }
-
-
-    const r = randomInt(0, positions.length / 3)
-    const al = this.PLANE_WIDTH * 1 / 4,
-      ar = this.PLANE_WIDTH * 3 / 4
-
-    const range = 15
-
-    const leftMountainIdx = [],
-      rightMountainIdx = []
-
-    for (var i = 0; i < this.PLANE_DEPTH; i++) {
-      const nal = Math.abs(simplex.noise2D(al, i * 0.1)),
-        nar = Math.abs(simplex.noise2D(ar, i * 0.1))
-
-      const idxl = i * this.PLANE_WIDTH + Math.floor(nal * range + 1) + al + i,
-        idxr = i * this.PLANE_WIDTH + Math.floor(nar * range + 1) + ar + i
-
-
-      leftMountainIdx.push(idxl)
-      rightMountainIdx.push(idxr)
-
-      const nbsl = randomInt(1, 3),
-        nbsr = randomInt(1, 3)
-
-      for (let j = -nbsl; j < nbsl; j++) {
-        leftMountainIdx.push(idxl + j)
-      }
-      for (let j = -nbsr; j < nbsr; j++) {
-        rightMountainIdx.push(idxr + j)
-      }
-    }
-    leftMountainIdx.forEach((idx, i) => {
-      positions[idx * 3 + 1] = Math.abs(simplex.noise2D(idx*0.1, i*0.1)) * MOUNTAIN_HEIGHT
-    })
-    rightMountainIdx.forEach((idx, i) => {
-      positions[idx * 3 + 1] = Math.abs(simplex.noise2D(idx*0.1, i*0.1)) * MOUNTAIN_HEIGHT
-    })
-
     const material = new THREE.MeshPhongMaterial({
       color: new THREE.Color(0xffebff),
       shading: THREE.FlatShading,
@@ -204,14 +161,34 @@ class Terrain {
     const mountain = new THREE.Mesh(geometry, material)
     this.planeMesh = mountain
 
-
-
     this.group.add(mountain)
+
+    for (var i = 0; i < this.PLANE_DEPTH; i++) {
+      //const nal = Math.abs(simplex.noise3D(20, i * 0.1, 0.1)),
+
+      const m = new Mountain({
+        height: conf.mountainHeight * this.MOUNTAIN_HEIGHT,
+        range: randomInt(4, 8),
+        zpos: this.PLANE_DEPTH + i,
+//        xpos: this.PLANE_WIDTH * 1 / 4,
+        anchor: this.PLANE_WIDTH * 1 / 4
+      })
+      this.mountains.push(m)
+
+      const mr = new Mountain({
+        height: conf.mountainHeight * this.MOUNTAIN_HEIGHT,
+        range: randomInt(4, 8),
+        zpos: this.PLANE_DEPTH + i,
+        //xpos: this.PLANE_WIDTH * 3 / 4,
+        anchor: this.PLANE_WIDTH * 3 / 4
+      })
+      this.mountains.push(mr)
+    }
 
   }
 
-  generateHeight(width, height) {
-    const seed = randomInt(10, 1000)
+  generateHeight(width, height, time) {
+    //const seed = randomInt(10, 1000)
 
     let size = width * height,
       data = new Float32Array(size)
@@ -219,37 +196,121 @@ class Terrain {
     for (let i = 0; i < size; i++) {
       const x = i % width,
         y = Math.floor(i / width)
-      data[i] = simplex.noise2D(seed + x * 0.2, y * 0.3)
+
+      data[i] = simplex.noise3D(x * 0.2, y * 0.3, time)
     }
     return data
   }
 
-  updateMountains(time) {
-    const height = 15 * conf.mountHeight
+  updateTerrain(time) {
+
+    const speed = conf.speed * 0.6
+
+    const height = this.MOUNTAIN_HEIGHT * conf.mountHeight,
+      terrainHeight = conf.terrainHeight * this.TERRAIN_HEIGHT
+      //console.log(time)
 
     const positions = this.planeMesh.geometry.attributes.position.array
 
-    for (var j = 0; j < this.PLANE_WIDTH; j++) {
-      const idx = (this.PLANE_DEPTH-1) * this.PLANE_WIDTH + j
-      console.log(idx)
-        positions[idx * 3 + 1] = 0
+    const data = this.generateHeight(this.PLANE_WIDTH, this.PLANE_DEPTH, time * 0.1)
+      //console.log(data)
+    for (var i = 0, j = 0; i < positions.length; i++, j += 3) {
+
+
+      const z = Math.floor(i / this.PLANE_WIDTH)
+      positions[j + 1] = simplex.noise2D(z * 0.05, time*speed) * 1
+
+
+      positions[j + 1] += data[i] * terrainHeight
     }
 
-    for (var i=0; i < this.PLANE_DEPTH-1; i++) {
-      for (var j=0; j < this.PLANE_WIDTH; j++) {
-        let idx = i*this.PLANE_WIDTH + j
-        let idxNext = (i+1)*this.PLANE_WIDTH + j
 
-        positions[idx*3 + 1] = positions[idxNext*3 + 1]
+
+    this.planeMesh.geometry.attributes.position.needsUpdate = true
+  }
+
+  updateMountains(time) {
+
+    const speed = conf.speed * 0.6
+
+    const mountainHeight = conf.mountainHeight * this.MOUNTAIN_HEIGHT
+
+    const positions = this.planeMesh.geometry.attributes.position.array
+
+    this.mountains.forEach((m, i) => {
+
+      m.setZPos(m.getZPos() - 0.2)
+      if (Math.floor(m.getZPos()) < this.PLANE_DEPTH) {
+        const idx = Math.floor(m.getZPos()) * this.PLANE_WIDTH + m.getXPos() + Math.floor(m.getZPos())
+
+        for (let j = -m.getRange(); j < m.getRange(); j++) {
+          positions[(idx + j) * 3 + 1] += m.getHeight(j + m.getRange())
+        }
+        if (m.getZPos() < 0) {
+          m.setZPos(this.PLANE_DEPTH)
+          m.setRange(randomInt(2, 8))
+          m.setMaxHeight(/*simplex.noise2D(i, time * 0.1) */ mountainHeight)
+          m.setXPos(m.getAnchor() + Math.floor(simplex.noise2D(m.getZPos(), time * speed)*5))
+        }
       }
-    }
+    })
 
+    this.planeMesh.geometry.attributes.position.needsUpdate = true
   }
 
   update(time) {
-    //this.updateMountains()
+    this.updateTerrain(time)
+    this.updateMountains(time)
   }
 
+}
+
+class Mountain {
+  constructor(args) {
+    this.height = args.height
+    this.zpos = args.zpos
+    this.xpos = args.xpos
+    this.anchor = args.anchor
+    this.heights = []
+    this.setRange(args.range)
+  }
+  getZPos() {
+    return this.zpos
+  }
+  getXPos() {
+    return this.xpos
+  }
+  getAnchor() {
+    return this.anchor
+  }
+  setXPos(xpos) {
+    this.xpos = xpos
+  }
+  setZPos(zpos) {
+    this.zpos = zpos
+  }
+  getRange() {
+    return this.range
+  }
+  setRange(range) {
+    this.range = range
+
+    this.heights = []
+    const seed = random(1, 100)
+    for (let i = 0; i < range * 2; i++) {
+      const factor = Math.sin(i/range*2)
+      this.heights.push(Math.abs(simplex.noise2D(i, seed)) * factor * this.height / 2 + this.height / 2)
+    }
+  }
+  setMaxHeight(height) {
+    this.height = height
+  }
+  getMaxHeight() {
+    return this.height
+  }
+  getHeight(i) {
+    return this.heights[i]
+  }
 }
 
 export default terrain
